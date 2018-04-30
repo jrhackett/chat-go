f00fdc49f6fd19badc500ea70959a2c626154efd
Jacob Hackett <jacobrhackett@gmail.com>
2018-04-30 19:38:46 -0400

    adding react client

---
+++ client/webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+// Constant with our paths
+const paths = {
+  DIST: path.resolve(__dirname, 'dist'),
+  SRC: path.resolve(__dirname, 'src'),
+};
+// Webpack configuration
+module.exports = {
+    entry: {
+        index: './src/index.jsx'
+    },
+    devServer: {
+        contentBase: paths.SRC,
+    },
+    resolve: {
+        extensions: ['.js', '.jsx'],
+    },
+    output: {
+        path: paths.DIST,
+        filename: 'app.bundle.js',
+    },
+    // Loaders configuration -> ADDED IN THIS STEP
+    // We are telling webpack to use "babel-loader" for .js and .jsx files
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: [
+                    'babel-loader',
+                ],
+            },
+        ],
+    },
+     // Tell webpack to use html plugin
+     plugins: [
+        new HtmlWebpackPlugin({
+            template: path.join(paths.SRC, 'index.html'),
+        }),
+    ],
+};
