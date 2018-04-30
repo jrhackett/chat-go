897499afc298241784dd36ea9ed9c0482b3e7681
Jacob Hackett <jacobrhackett@gmail.com>
2018-04-30 19:38:54 -0400

    removing an old log

--- main.go
+++ main.go
@@ -76,7 +76,6 @@ func handleMessages() {
 		for client := range clients {
 			err := client.WriteJSON(msg)
 			if err != nil {
-				log.Printf("error: %v", err)
 				client.Close()
 				delete(clients, client)
 			}
