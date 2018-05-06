553df3f32b1c38cd7072f7de18b23e8285ec39d8
Jacob Hackett <jacobrhackett@gmail.com>
2018-05-05 23:20:26 -0400

    styles for input bar

--- client/components/chat/MessageInputBar.jsx
+++ client/components/chat/MessageInputBar.jsx
@@ -1,5 +1,44 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { css } from 'react-emotion'
+
+const messageBarSizing = css`
+    padding: 0.5rem 0.5rem;
+    font-size: 1rem;
+`
+
+const container = css`
+    position: fixed;
+    bottom: 0;
+    width: 100%;
+
+    &> form {
+        width: 100%;
+        display: flex;
+        margin: 0;
+    }
+`
+
+const messageInput = css`
+    ${ messageBarSizing }
+
+    flex-basis: 95%;
+    border: 0;
+    border-top: 1px solid #cecece;
+
+    &:focus {
+        outline: none;
+    }
+`
+
+const messageButton = css`
+    ${ messageBarSizing }
+
+    flex-basis: 5%;
+    border: 0;
+    border-left: 1px solid #cecece;
+    border-top: 1px solid #cecece;
+`
 
 class MessageInputBar extends React.Component {
     constructor() {
@@ -23,10 +62,10 @@ class MessageInputBar extends React.Component {
 
     render() {
         return (
-            <div>
+            <div className={ container }>
                 <form onSubmit={ () => false }>
-                    <input onChange={ e => this.onInputChange("message", e.target.value) } />
-                    <button>Send</button>
+                    <input className={ messageInput } onChange={ e => this.onInputChange("message", e.target.value) } />
+                    <button className={ messageButton }>Send</button>
                 </form>
             </div>
         )
